bc097f6769ccc7d5f4733d3b2277eb1e6ddf1643
Jaesung Jung <jaesung.jung1986@gmail.com>
2025-01-27 15:30:42 +0900

    Add VisualEffect

---
+++ Sources/GlassUI/VisualEffect.swift
@@ -0,0 +1,17 @@
+//
+//  VisualEffect.swift
+//  GlassUI
+//
+//  Created by 정재성 on 1/27/25.
+//
+
+import SwiftUI
+
+struct VisualEffect: UIViewRepresentable {
+  func makeUIView(context: Context) -> some UIView {
+    return UIVisualEffectView()
+  }
+
+  func updateUIView(_ uiView: UIViewType, context: Context) {
+  }
+}
